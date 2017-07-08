af1810a51a7bdd5100784798f866b930d3f28391
megans <megan.swanby@rga.com>
2017-07-08 11:33:54 -0400

    Transfer jquery commands to vanilla js.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 // You’ll read in that JSON file using JavaScript and display the results on your web app.
 // If available, your web app will also use the Web Speech API (available in Google Chrome 33 and above) to respond back to you verbally.
 
-const accessToken = ''; //client
-const subscriptionKey = ''; //dev key
+const accessToken = '09f082fa3d154dd081fdd27dd2360576'; //client
+const subscriptionKey = '47a5893f66844728aec2fcfd7c9a6d39'; //dev key
 const baseUrl = 'https://api.api.ai/v1/';
 let $speechInput;
 let $recordBtn;
@@ -14,24 +14,34 @@ const messageRecording = "Recording...";
 const messageCouldntHear = 'I couldn\'t hear that. Try again.';
 const messageInternalError = 'Oops. Something is wrong internally. Getting an error';
 const  messageSorry = 'I\'m sorry. I can\'t answer that for you.';
+const speechInput = document.getElementById('speech');
+const recordBtn = document.getElementById('record');
 
+// $(document).ready(function() {
+  // $speechInput = $('#speech');
+  // $recordBtn = $("#record");
+  // let recognition;
 
-$(document).ready(function() {
-  $speechInput = $('#speech');
-  $recordBtn = $("#record");
-  let recognition;
-
-  $speechInput.keypress(function(event) {
+  speechInput.addEventListener("keypress", function() {
     if(event.which == 13) { //enter
       event.preventDefault();
       send()
     }
   });
-
-  // turns on/off speech recog
-  $('#record').click(function(event) {
+  // speechInput.keypress(function(event) {
+  //   if(event.which == 13) { //enter
+  //     event.preventDefault();
+  //     // send()
+  //     console.log(event.target);
+  //   }
+  // });
+  recordBtn.addEventListener('click', function(event) {
     switchRecognition();
   })
+  // // turns on/off speech recog
+  // $('#record').click(function(event) {
+  //   switchRecognition();
+  // })
 
   function switchRecognition() {
     if(recognition) {
@@ -77,36 +87,44 @@ $(document).ready(function() {
   }
 
   function updateRecord() {
-    $recordBtn.text(recognition ? 'STOP' : 'SPEAK');
+    recordBtn.text(recognition ? 'STOP' : 'SPEAK');
   }
 
   function setInput(text) {
-    $('input').val(text);
+    console.log(text);
+    // ('input').val(text);
     send();
   }
 
   function send() {
-    let text = $speechInput.val();
+    let text = speechInput.value;
     console.log(`sent this text: ${text}`);
-
-    $.ajax({
-      type: "POST",
-      url: baseUrl + "query",
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      headers: {
-        "Authorization": `Bearer ${accessToken}`,
-        "ocp-apim-subscription-key": subscriptionKey
-      },
-      data: JSON.stringify({query: text, lang: "en", sessionId: "1234567890"}),
-
-      success: function(data) {
-        prepareResponse(data);
-      },
-      error: function() {
-        respond(messageInternalError);
-      }
-    });
+    const url = `${baseUrl}query`;
+    const request = new XMLHttpRequest();
+    let data = JSON.stringify({query: text, lang: "en", sessionId: "1234567890"});
+    console.log(url);
+    request.open("POST", url, true);
+    request.setRequestHeader("Authorization", `Bearer ${accessToken}`)
+    request.send(data)
+    //       "ocp-apim-subscription-key": subscriptionKey)
+  //   $.ajax({
+  //     type: "POST",
+  //     url: baseUrl + "query",
+  //     contentType: "application/json; charset=utf-8",
+  //     dataType: "json",
+  //     headers: {
+  //       "Authorization": `Bearer ${accessToken}`,
+  //       "ocp-apim-subscription-key": subscriptionKey
+  //     },
+  //     data: JSON.stringify({query: text, lang: "en", sessionId: "1234567890"}),
+  //
+  //     success: function(data) {
+  //       prepareResponse(data);
+  //     },
+  //     error: function() {
+  //       respond(messageInternalError);
+  //     }
+  //   });
   }
 
   function prepareResponse(val) {
@@ -129,6 +147,9 @@ $(document).ready(function() {
       window.speechSynthesis.speak(msg);
     }
 
-    $("#spokenResponse").addClass("is-active").find('.spoken-response__text').html(val);
+    document.getElementById('spokenResponse')
+      .addClass("is-active")
+      .find('.spoken-response__text')
+      .html(val);
   }
-});
+// });
