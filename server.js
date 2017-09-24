c9d117f415493172f4344b3cb8159fd7922b264e
NYCSwan <megan.swanby@gmail.com>
2017-09-24 15:41:26 -0400

    Change / route to send index

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 
 // init project
 const express = require('express');
+const path = require('path');
 const app = express();
-
 
 app.use(express.static('public'));
 
-app.get("/", function (request, response) {
-  response.send('index');
+app.get("/", function(request, response) {
+  response.sendFile(path.join(__dirname+'/index.html'));
 });
 
 // listen for requests :)
